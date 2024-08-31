6b161a128966590e90967a7769af20144d13796b
Christian Estevam <christianestevamb@gmail.com>
2024-08-30 23:54:33 -0300

    feat: configuração da camada de segurança e serviço de autenticação jwt

---
+++ src/main/java/com/ufc/easydesk/service/MyUserDetails.java
@@ -0,0 +1,55 @@
+package com.ufc.easydesk.service;
+
+import com.ufc.easydesk.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class MyUserDetails implements UserDetails {
+
+    private Usuario usuario;
+
+    public MyUserDetails(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return usuario.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getNome()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return usuario.getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/ufc/easydesk/service/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.ufc.easydesk.service;  // Ajuste o pacote conforme sua estrutura
+
+import com.ufc.easydesk.model.Usuario;
+import com.ufc.easydesk.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Usuario> user = usuarioRepository.findByEmail(username);
+
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+
+        return new MyUserDetails(user.get());
+    }
+}
