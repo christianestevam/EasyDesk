94f6331476156a79bff38187b8d19fc7dc8e6895
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2024-09-13 15:50:51 -0300

    feat: add get all comandas

--- src/main/java/com/ufc/easydesk/domain/repository/ComandaRepository.java
+++ src/main/java/com/ufc/easydesk/domain/repository/ComandaRepository.java
@@ -3,5 +3,9 @@ package com.ufc.easydesk.domain.repository;
 import com.ufc.easydesk.domain.model.Comanda;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ComandaRepository extends JpaRepository<Comanda, Long> {
+
+    List<Comanda> findByAtivaTrue();
 }
