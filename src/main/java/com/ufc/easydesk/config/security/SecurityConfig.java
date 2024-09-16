7bf8dbb5419866620fd4b9afa7b689f78de698c7
Christian Estevam <christianestevamb@gmail.com>
2024-09-16 19:37:59 -0300

    config: endereço da url do front-end

--- src/main/java/com/ufc/easydesk/config/security/SecurityConfig.java
+++ src/main/java/com/ufc/easydesk/config/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://easy-desk-app-7a29dfc84903.herokuapp.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
